2fa5235f2640ce9cbb43579492d4559b8b64d082
Waheed <waheed.ahmad01@gmail.com>
2019-02-07 16:40:06 +0500

    Update application.py

--- application.py
+++ application.py
@@ -6,47 +6,13 @@ from flask import (
 
 app = Flask(__name__)
 
-plans = [
-    {'planId': 1,
-     'projectId': '1',
-     'storeId': '1',
-     'projectDuration': '1 Weeks',
-     'filename': 'S1P1W1.phs',
-	 'versionNmbr': 'v1',
-	 'activeflag': 'true',
-	 'createdByUser': 'ABC'	 ,
-	 "createdDate": "2018-11-16"
-	 },
-    {'planId': 3,
-     'projectId': '1',
-     'storeId': '1',
-     'projectDuration': '2 Weeks',
-     'filename': 'S1P1W2.phs',
-	 'versionNmbr': 'v1',
-	 'activeflag': 'true',
-	 'createdByUser': 'ABC'	 ,
-	 "createdDate": "2018-11-16"
-	 },
-    {'planId': 3,
-     'projectId': '1',
-     'storeId': '1',
-     'projectDuration': '3 Weeks',
-     'filename': 'S1P1W3.phs',
-	 'versionNmbr': 'v1',
-	 'activeflag': 'true',
-	 'createdByUser': 'ABC'	,
-	 "createdDate": "2018-11-16"
-	 },
-]
 
 @app.route('/', methods=['GET'])
 
 def home():
     return "OK"
 
-@app.route('/all', methods=['GET'])
-def api_all():
-    return jsonify(plans)
+
 @app.route('/plans', methods = ['POST'])
 def api_message():
 
@@ -64,25 +30,3 @@ def api_message():
 
     else:
         return "415 Unsupported Media Type ;)"
-@app.route('/BuildPlan', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for plan in plans:
-        if plan['planId'] == id:
-            results.append(plan)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(id)
